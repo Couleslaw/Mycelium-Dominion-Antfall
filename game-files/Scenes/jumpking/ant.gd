b0eef191a90201d681d2328e99cab8cdea88d476
Adam Paseka <pasekaadam@gmail.com>
2024-03-09 00:06:19 +0100

    do details

--- game-files/Scenes/jumpking/ant.gd
+++ game-files/Scenes/jumpking/ant.gd
@@ -2,7 +2,7 @@ extends CharacterBody2D
 
 
 const SPEED = 300.0
-const JUMP_VELOCITY = -300.0
+const JUMP_VELOCITY = -500.0
 @onready var sprite = $AnimatedSprite2D
 
 # Get the gravity from the project settings to be synced with RigidBody nodes.
@@ -35,4 +35,4 @@ func _physics_process(delta):
 	move_and_slide()
 
 func catapult():
-	velocity.y = JUMP_VELOCITY*3
+	velocity.y = JUMP_VELOCITY*2
