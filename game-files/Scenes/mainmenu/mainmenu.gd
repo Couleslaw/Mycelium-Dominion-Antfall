22ba906ff681eb93e6d03bcb07b56dec377fae84
Adam Paseka <pasekaadam@gmail.com>
2024-03-09 18:27:40 +0100

    add shrooms to start

--- game-files/Scenes/mainmenu/mainmenu.gd
+++ game-files/Scenes/mainmenu/mainmenu.gd
@@ -1,24 +1,8 @@
 extends Control
 
 
-# Called when the node enters the scene tree for the first time.
-func _ready():
-	pass # Replace with function body.
+func _on_end_shroom_pressed():
+	get_tree().quit()
 
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta):
-	pass
-
-
-
-func _on_quit_button_gui_input(event):
-	if event is InputEventMouseButton:
-		if event.button_index == MOUSE_BUTTON_LEFT and event.pressed:
-			get_tree().quit()
-
-
-func _on_start_button_gui_input(event):
-	if event is InputEventMouseButton:
-		if event.button_index == MOUSE_BUTTON_LEFT and event.pressed:
-			get_tree().change_scene_to_file("res://Scenes/mainmenu/levelmap.tscn")
+func _on_start_shroom_pressed():
+	get_tree().change_scene_to_file("res://Scenes/mainmenu/levelmap.tscn")
